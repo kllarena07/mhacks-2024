08fed36aa13e7c8806310ad2934c0e0801b13405
Gabriel Push <gabepush.college@gmail.com>
2024-09-28 13:45:40 -0400

    iOS

---
+++ MHacks24_iOS/MHacks24_iOS/MHacks24_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  MHacks24_iOSApp.swift
+//  MHacks24_iOS
+//
+//  Created by Gabriel Push on 9/28/24.
+//
+
+import SwiftUI
+
+@main
+struct MHacks24_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MHacks24_iOS/MHacks24_iOSTests/MHacks24_iOSTests.swift
@@ -0,0 +1,17 @@
+//
+//  MHacks24_iOSTests.swift
+//  MHacks24_iOSTests
+//
+//  Created by Gabriel Push on 9/28/24.
+//
+
+import Testing
+@testable import MHacks24_iOS
+
+struct MHacks24_iOSTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
