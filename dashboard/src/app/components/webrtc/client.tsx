30fed866ae0b582a1cd4fe9900c3ed4c6d1d9a77
Kieran Llarena <baymax709@gmail.com>
2024-09-29 09:05:00 -0400

    chore: cmon vercel

--- dashboard/src/app/components/webrtc/client.tsx
+++ dashboard/src/app/components/webrtc/client.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useRef } from "react";
 import io, { Socket } from "socket.io-client";
 // import GoogleMap from "../map/map";
 
