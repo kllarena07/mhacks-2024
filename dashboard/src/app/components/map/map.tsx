0f156d7751472af58a08799e3fbeaac90b0ec3ca
Kieran Llarena <baymax709@gmail.com>
2024-09-29 05:58:00 -0400

    hotfix

--- dashboard/src/app/components/map/map.tsx
+++ dashboard/src/app/components/map/map.tsx
@@ -1,42 +1,42 @@
-"use client";
+// "use client";
 
-import React, { useEffect, useRef } from "react";
-import { Loader } from "@googlemaps/js-api-loader";
+// import React, { useEffect, useRef } from "react";
+// import { Loader } from "@googlemaps/js-api-loader";
 
-type MapProps = {
-  latitude: number;
-  longitude: number;
-};
+// type MapProps = {
+//   latitude: number;
+//   longitude: number;
+// };
 
-const GoogleMap = ({ latitude, longitude }: MapProps) => {
-  const mapRef = useRef(null);
+// const GoogleMap = ({ latitude, longitude }: MapProps) => {
+//   const mapRef = useRef(null);
 
-  useEffect(() => {
-    const initMap = async () => {
-      const loader = new Loader({
-        apiKey: process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY ?? "",
-        version: "weekly",
-      });
+//   useEffect(() => {
+//     const initMap = async () => {
+//       const loader = new Loader({
+//         apiKey: process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY ?? "",
+//         version: "weekly",
+//       });
 
-      const { Map } = await loader.importLibrary("maps");
+//       const { Map } = await loader.importLibrary("maps");
 
-      const mapOptions = {
-        center: { lat: latitude, lng: longitude },
-        zoom: 14,
-      };
+//       const mapOptions = {
+//         center: { lat: latitude, lng: longitude },
+//         zoom: 14,
+//       };
 
-      const map = new Map(mapRef.current, mapOptions);
+//       const map = new Map(mapRef.current, mapOptions);
 
-      new google.maps.Marker({
-        position: { lat: latitude, lng: longitude },
-        map: map,
-      });
-    };
+//       // new google.maps.Marker({
+//       //   position: { lat: latitude, lng: longitude },
+//       //   map: map,
+//       // });
+//     };
 
-    initMap();
-  }, [latitude, longitude]);
+//     initMap();
+//   }, [latitude, longitude]);
 
-  return <div ref={mapRef} className="w-full h-[400px]" />;
-};
+//   return <div ref={mapRef} className="w-full h-[400px]" />;
+// };
 
-export default GoogleMap;
+// export default GoogleMap;
